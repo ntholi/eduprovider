3973f2da55bfd435ff8ae7407640094343bf3635
ntholi <ntholi@live.com>
2017-05-16 10:46:57 +0200

    Create tutor_home and student_home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_tutor!
+
+  def after_sign_in_path_for(resource)
+    if resource.class == Tutor
+      admin_root_path
+    elsif resource.class == Student
+      dashboard_index_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :students
   devise_for :tutors
   resources :user_profiles
-  
+
+  get 'home/student_home'
+  get 'home/tutor_home'
   get 'home/index'
+  
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
