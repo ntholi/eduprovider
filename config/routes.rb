050920a6a90ddb5d084907c41a3846d58796926e
ntholi <ntholi@live.com>
2017-05-16 10:18:35 +0200

    Generate home index controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :tutors
   resources :user_profiles
+  
+  get 'home/index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
