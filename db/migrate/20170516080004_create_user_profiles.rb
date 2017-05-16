ed816ad70c353081a8c0257e89d29dd742f79be9
ntholi <ntholi@live.com>
2017-05-16 10:00:22 +0200

    scaffold user_profile

---
+++ db/migrate/20170516080004_create_user_profiles.rb
@@ -0,0 +1,16 @@
+class CreateUserProfiles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_profiles do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.string :street_name
+      t.string :city
+      t.string :country
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
