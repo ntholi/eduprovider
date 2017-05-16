2ca3da04027b9e70866e5fcf6b55ecb34ebeebc7
ntholi <ntholi@live.com>
2017-05-16 10:15:33 +0200

    Make tutor account confirmable

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,6 +1,8 @@
 class Tutor < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+  belongs_to :user_profile, optional: true
 end
