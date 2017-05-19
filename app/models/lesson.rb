9d004cde1a87e37132e77b7516dd06d3928fd5e7
ntholi <ntholi@live.com>
2017-05-19 12:36:43 +0200

    Generate Uploader

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,4 @@
 class Lesson < ApplicationRecord
   belongs_to :tutored_course
+  has_many :lessons
 end
--- app/models/video_lesson.rb
+++ app/models/video_lesson.rb
@@ -1,3 +1,4 @@
 class VideoLesson < ApplicationRecord
   belongs_to :lesson
+  mount_uploader :video, VideoUploader
 end
