342333d7e94985cf80b2f2dae8bad4631d1f66b3
ntholi <ntholi@live.com>
2017-05-22 15:45:49 +0200

    Disable confirmable in tutor

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -2,8 +2,8 @@ class Tutor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+        #  :confirmable
   belongs_to :user_profile, optional: true
   has_many :tutored_courses
 
