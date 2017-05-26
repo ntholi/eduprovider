82cb97a3fa7d024270603bbc39d3717903743fc1
ntholi <ntholi@live.com>
2017-05-26 15:57:25 +0200

    validates precense of video and audio

--- app/models/audio_lesson.rb
+++ app/models/audio_lesson.rb
@@ -2,4 +2,7 @@ class AudioLesson < ApplicationRecord
   belongs_to :lesson
   has_attached_file :audio
   validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
+  validates_presence_of :audio
+
+  process_in_background :audio
 end
--- app/models/video_lesson.rb
+++ app/models/video_lesson.rb
@@ -9,6 +9,7 @@ class VideoLesson < ApplicationRecord
     :thumb => { :geometry => "160x120", :format => 'jpeg', :time => 10}
     }, :processors => [:transcoder]
     validates_attachment_content_type :video, content_type: /\Avideo\/.*\Z/
+    validates_presence_of :video
 
     process_in_background :video
 end
