f3a7a434d6c0199bd2afd1cf8a46c1c9e60e754c
ntholi <ntholi@live.com>
2017-05-26 14:04:30 +0200

    Delete video when deleting video_lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,8 +1,8 @@
 class Lesson < ApplicationRecord
   belongs_to :tutored_course
-  has_one :video_lesson
+  has_one :video_lesson, :dependent => :delete_all
   validates_presence_of :title
-  
+
   def to_s
     title
   end
