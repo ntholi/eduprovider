9866f0830ce1abe8f96e81e763b99b3b6bc91d18
ntholi <ntholi@live.com>
2017-06-16 15:09:26 +0200

    question has many answers

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,7 @@ class Lesson < ApplicationRecord
   belongs_to :tutored_course
   has_one :video_lesson, :dependent => :destroy
   has_one :audio_lesson, :dependent => :destroy
+  has_many :questions
   validates_presence_of :title
 
   def to_s
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :student
   belongs_to :lesson
+  has_many :answers
 end
