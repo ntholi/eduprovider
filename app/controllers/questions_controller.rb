c5177445437c2700af4dacb9024a15330d8ea269
ntholi <ntholi@live.com>
2017-06-20 09:13:34 +0200

    Add privacy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -75,6 +75,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title, :content, :student_id, :lesson_id)
+      params.require(:question).permit(:title, :content, :student_id, :lesson_id, :privacy)
     end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ApplicationRecord
   belongs_to :student
   belongs_to :lesson
   has_many :answers
+
 end
