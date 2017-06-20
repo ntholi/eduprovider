b52fb8b0b26a7cceefdcff5617d7d1fcf3de55f0
ntholi <ntholi@live.com>
2017-06-20 08:35:53 +0200

    Cannot enroll course more than once

--- app/models/student_enrollment.rb
+++ app/models/student_enrollment.rb
@@ -2,6 +2,8 @@ class StudentEnrollment < ApplicationRecord
   belongs_to :student
   belongs_to :tutored_course
 
+  validates_uniqueness_of :tutored_course, message: 'You are already enrolled in this course'
+
   def to_s
     tutored_course.name_with_tutor
   end
